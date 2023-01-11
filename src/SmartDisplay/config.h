b3a7345a024ed25c8cb548344e2fb584a34569d6
Brun <git.s1@fiz-o-matic.net>
2023-01-11 20:21:56 +0100

    added trip reset function

--- src/SmartDisplay/config.h
+++ src/SmartDisplay/config.h
@@ -14,7 +14,7 @@
  * mendatory hardware defination
  */
 
-//#define DEBUG
+//#define DEBUG // this disables GPS on the Serial Port
 //#define HW_ALPHA_OLED
 //#define HW_ALPHA_LCD
 
@@ -23,5 +23,11 @@
  * optional configuration
  */
 
-// the display goint into the sleep mode
+// Time to disable the Display in Standby mode
 #define DISPLAY_TIMEOUT 600000 // 60s
+
+// Voltage value to turn on
+#define TURN_ON_VOLTAGE 13.5
+
+// Calibrate the Voltage 
+#define VOLTAGE_CALIBRATION 0.9
